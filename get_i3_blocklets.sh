f3c501d33eaea4be135fbc064c1e1c55fd1e7b6c
Atanas <atanasormanow@gmail.com>
2021-03-19 16:21:00 +0200

    Let the script be only for i3blocklets

--- get_i3_blocklets.sh
+++ get_i3_blocklets.sh
@@ -2,10 +2,11 @@
 
 if [ -z "$1" ]
   then
-    DIR=$HOME/Repos
+    DIR=$HOME/Repos/i3blocks-contrib
     mkdir $DIR
     git clone https://github.com/vivien/i3blocks-contrib $DIR
   else
+    echo "Make sure to update your i3blocks config file with the location of your blocklets!"
     git clone https://github.com/vivien/i3blocks-contrib $1
 fi
 
